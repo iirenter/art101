285509b6224936f6e3d614db840167784ca93f54
iirenter <iirenter@ucsc.edu>
2021-10-18 17:12:17 -0700

    Lab5 Done

---
+++ lab4/js/lab.js
@@ -0,0 +1,15 @@
+// Summary. A calcualtor that does basic math problems.
+// Description. The program takes in two numbers and an operation from the user then caculates the result.
+// @link url
+// @flie This file containes pseudo code for making a basic calculator.
+// @author Israel Renteria
+// @since 10.13.21
+
+// Ask the user to input a number
+// Take in user input
+// Ask the user to input an operation
+// Take in user input
+// Ask user to input a second number
+// Take in user input
+// Calculate the result
+// Output/print the result
---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+/*
+*author. Israel Renteria
+*Date Created. 10/18/21
+*/
+
+//Define Varables
+var make = "Ford";
+var model = "Taurus";
+var color = "Green";
+var year = 2004;
+var age;
+
+// Calculate age
+age = 2021- year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: "+ year + "<br>");
