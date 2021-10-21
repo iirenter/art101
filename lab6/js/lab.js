6dd9b7b932eb5917ade5a5232b88449dff932a1a
iirenter <iirenter@ucsc.edu>
2021-10-20 17:53:15 -0700

    Lab6

---
+++ lab6/js/lab.js
@@ -0,0 +1,21 @@
+/*
+*author. Israel Renteria
+*Date Created. 10/20/21
+*/
+
+//Declaring array and Objects
+var myTransports = ["walk", "bike", "bus", "Ford Taurus"];
+
+var myMainRide = {
+  make : "Ford",
+  model : "Taurus",
+  year : 1994,
+  age : function() {
+    return 2021 - year;
+  }
+};
+
+//Output
+document.writeln("Getting around on: " + myTransports + "<br>");
+document.writeln("My Main Ride : <pre>",
+JSON.stringify(myMainRide, null, '\t'), "</pre>");
