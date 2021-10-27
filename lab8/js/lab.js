141bbd5bd2f9d603378af38b389bb765a85a48a9
iirenter <iirenter@ucsc.edu>
2021-10-27 12:56:09 -0700

    Finished task x

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -27,3 +27,10 @@ var result= array.map(function(x){
     return results;
 });
 console.log("My array plus 5: ", result);
+
+//Task x a function of my own creation that takes a callback as a parameter
+function doubleSquare(sqrNumber, y){
+  var result = sqrNumber(y) * 2;
+  return result;
+}
+console.log("Doubling the square of 6 is" ,doubleSquare(sqrNumber, 6));
