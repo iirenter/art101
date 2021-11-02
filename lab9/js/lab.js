b2518a37fed010c074c85b4693cabc8e071e606c
iirenter <iirenter@ucsc.edu>
2021-11-01 20:03:22 -0700

    Lab 9 done

---
+++ lab9/js/lab.js
@@ -0,0 +1,19 @@
+/*
+* author. Israel Renteria
+* date. 11/1/21
+*/
+
+// assign the content of div output to a varable
+var outputEl = document.getElementById("output");
+
+//ceate a new element and assign it a varable
+var new1El = document.createElement('p');
+new1El.innerHTML= "This should work right?";
+
+var new2El = document.createElement('p');
+new2El.innerHTML = "I got this for sure";
+
+//append outputEl
+
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
